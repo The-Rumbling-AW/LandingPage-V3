0dcb91a193aacb3b155af02c02416d32be65a674
antuanete <u202310358@upc.edu.pe>
2025-06-22 20:16:38 -0500

    Subida completa desde local

---
+++ src/main.js
@@ -0,0 +1,36 @@
+//import
+
+import { createApp } from 'vue'
+
+import './style.css'
+
+import App from './App.vue'
+
+import PrimeVue from 'primevue/config';
+
+import Aura from '@primevue/themes/aura'
+
+import {SelectButton} from "primevue";
+
+import i18n from "./i18n.js";
+
+import router from "./shared/router/index.js";
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.component('pv-select-button', SelectButton)
+
+app.use(i18n);
+
+app.use(router);
+
+app.mount('#app');
+
